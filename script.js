9a31c69d8405c811d4753d1fd4e86c9269ea1270
Kack for_Learning <Kacking1011@gmail.com>
2022-12-28 13:49:02 +0300

    total adjustments to all files

---
+++ script.js
@@ -0,0 +1,18 @@
+const timerLabel = document.getElementById('label');
+const timerInput = document.getElementsByClassName('inputt');
+const setButton = document.getElementsByClassName('buttonn');
+
+
+// دالة تحسب الرقم الموجود فوق
+
+//دالة تخزن الرقم الجديد
+
+// دالة تتعامل مع رز الايقاف وهو نفسه زر ابدأ
+
+// دالة زر ابدأ
+
+
+
+
+
+//دالة main الاساسية
